aa6356d72f0403ff8e1d5269325d112cf945d82f
Bruno Lavoie <bl@brunol.com>
2016-06-30 14:32:20 -0400

    Add filebeat 5-a4 to test.

--- run.cmd
+++ run.cmd
@@ -9,8 +9,8 @@ del /q registry.*
 curl -XDELETE http://ul-rvl-pr-ela01.ul.ca:9200/ul-dbg-filebeat-*
 
 :: run filebeat
-start filebeat-1.2.3.exe -c filebeat.windows.yml -e -v 
-::start filebeat.exe -c filebeat.windows.yml -e  -d "*"
+start filebeat-1.2.3.exe        -c filebeat-1.2.3.yml        -e -v
+start filebeat-5.0.0-alpha4.exe -c filebeat-5.0.0-alpha4.yml -e -v
 
 :: start generator
 cd generator-bin
